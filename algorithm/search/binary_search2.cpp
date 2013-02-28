5a5b98f4d692acab99a5ba53b013155edf46d1ab
mryoshio <yoshiokaas@gmail.com>
2013-02-28 20:18:23 +0900

    added binary_search 1 & 2 in cpp

--- algorithm/search/binary_search2.cpp
+++ algorithm/search/binary_search2.cpp
@@ -9,17 +9,17 @@ using namespace std;
 void binary_search(int t, int *array) {
     cout << "search " << t << endl;
     int l = 0, r = N-1, m;
-    while (l <= r) {
+    while (l < r) {
         m = (l + r) / 2;
-        if (array[m] == t) {
-            cout << t << " found at " << m + 1 << endl;
-            return;
-        }
         if (array[m] < t) {
             l = m + 1;
         } else {
-            r = m - 1;
+            r = m;
         }
+    }
+    if (array[l] == t) {
+        cout << t << " found at " << l + 1 << endl;
+        return;
     }
     cout << "not found" << endl;
 }

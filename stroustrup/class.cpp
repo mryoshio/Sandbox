49c869794e7415d97434b7b9f09ebe5e097706ee
yoshiokaas <yoshiokaas@gmail.com>
2013-03-31 17:32:59 +0900

    created Year class
    changed month to use Enum

--- stroustrup/class.cpp
+++ stroustrup/class.cpp
@@ -2,27 +2,46 @@
 
 using namespace std;
 
-class Date {
+class Year {
+    static const int min = 1800;
+    static const int max = 2200;
 public:
     class Invalid {};
-    Date(int yy, int mm, int dd) : y(yy), m(mm), d(dd) {
-        if (! check()) throw Invalid();
+    Year(int x) : y(x) { if (x < min || max <= x) throw Invalid(); }
+    int year() { return y; }
+private:
+    int y;
+};
+
+class Date {
+public:
+    enum Month {
+        jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+    };
+    class Invalid {};
+    Date(Year yy, Month mm, int dd) : y(yy), m(mm), d(dd) {
+        if (! check()) {
+            cout << "invalid input for Date." << endl;
+            throw Invalid();
+        }
     }
 
     void add_day(int n) {
         cout << "add day" << endl;
     }
-    int year() { return y; }
-    int month() { return m; }
+    Year year() { return y; }
+    Month month() { return m; }
     int day() { return d; }
-    string format() { 
+    string format() {
         char buf[10];
-        sprintf(buf, "%d/%d/%d", y, m, d);
+        sprintf(buf, "%d/%d/%d", y.year(), m, d);
         return buf;
     }
     
 private:
-    int y, m, d;
+    Year y;
+    Month m;
+    int d;
     bool check();
 };
 
@@ -33,9 +52,10 @@ bool Date::check() {
 
 int main() {
     try {
-        Date d(1999, 12, 1);
+        Date d(1999, Date::mar, 1);
         cout << d.format() << endl;
-        Date e(1999, 13, 1);
+        Date e(1999, Date::sep, 1);
+        Date f(1700, Date::sep, 1);
     } catch(Date::Invalid) {
         cout << "Invalid thrown" << endl;
     }

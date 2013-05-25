8cda899c1656ba425ed6d5ca2274f63212375864
mryoshio <ashitaba.yoshioka@leonisand.co>
2013-05-25 14:29:30 +0900

    arrange directory structure
    intro dynamic dispatcher in metaprogurammingruby

--- meta_programming_ruby/chapter2/computer.rb
+++ meta_programming_ruby/chapter2/computer.rb
@@ -2,6 +2,7 @@
 require './ds'
 
 class Computer
+
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
@@ -15,10 +16,15 @@ class Computer
     component :cpu
   end
 
+  def keyboard
+    component :keyboard
+  end
+
   def component(name)
     info = @data_source.send "get_#{name}_info", @id
     price = @data_source.send "get_#{name}_price", @id
-    return name if price >= 100
+    result = "#{name.to_s.capitalize}: #{info} (#{price})"
+    return "* #{result}" if price >= 100
   end
 end
 

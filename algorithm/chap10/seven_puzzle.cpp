2273718d492af78a3ed7e55c539b0879f6a5fee1
mryoshio <yoshiokaas@gmail.com>
2013-06-24 01:49:57 +0900

    refactoring DFS & BFS

--- algorithm/chap10/seven_puzzle.cpp
+++ algorithm/chap10/seven_puzzle.cpp
@@ -13,27 +13,22 @@ int history_count = 0;
 int queue_bottom;
 
 unsigned long make_hash(char* pattern) {
-  unsigned long hash;
-  int i;
+  unsigned long hash = 0;
 
-  hash = 0;
-  for (i = 0; i < 8; i++)
+  for (int i = 0; i < 8; i++)
     hash |= ((unsigned long)pattern[7 - i]) << (i * 4);
   return hash;
 }
 
 void pattern_from_hash(char* pattern, unsigned long hash) {
-  int i;
-  for (i = 0; i < 8; i++)
+  for (int i = 0; i < 8; i++)
     pattern[7 - i] = (char)((hash >> (i * 4)) & 0xf);
 }
 
 void save_history(char* pattern, int pattern_from) {
-  int i;
-  unsigned long hash;
+  unsigned long hash = make_hash(pattern);
 
-  hash = make_hash(pattern);
-  for (i = 0; i < history_count; i++)
+  for (int i = 0; i < history_count; i++)
     if (history[i].hash == hash)
       return;
 
@@ -41,7 +36,7 @@ void save_history(char* pattern, int pattern_from) {
   history = (PATTERN*)realloc(history, sizeof(PATTERN) * history_count);
   
   if (history == NULL) {
-    cout << "Out of Memory";
+    cout << "Out of Memory" << endl;
     exit(EXIT_FAILURE);
   }
 
@@ -68,26 +63,28 @@ int solve(void) {
     }
     
     if (blank > 3) {
-      pattern[blank] = pattern[blank - 4];
-      pattern[blank - 4] = 0;
-      save_history(pattern, queue_bottom);
+      // swap blank tile with upper one
+      swap(pattern[blank], pattern[blank - 4]);
+      // save new panel positions
+      save_history(pattern, queue_bottom); 
+      // revert to original positions for subsequent code
       pattern_from_hash(pattern, hash);
     }
     if (blank < 4) {
-      pattern[blank] = pattern[blank + 4];
-      pattern[blank + 4] = 0;
+      // swap blank tile with bottom one
+      swap(pattern[blank], pattern[blank + 4]);
       save_history(pattern, queue_bottom);
       pattern_from_hash(pattern, hash);
     }
     if (blank != 0 && blank != 4) {
-      pattern[blank] = pattern[blank - 1];
-      pattern[blank - 1] = 0;
+      // swap blank tile with right one
+      swap(pattern[blank], pattern[blank - 1]);
       save_history(pattern, queue_bottom);
-      pattern_from_hash(pattern, hash);
+            pattern_from_hash(pattern, hash);
     }
     if (blank != 3 && blank != 7) {
-      pattern[blank] = pattern[blank + 1];
-      pattern[blank + 1] = 0;
+      // swap blank tile with left one
+      swap(pattern[blank], pattern[blank + 1]);
       save_history(pattern, queue_bottom);
       pattern_from_hash(pattern, hash);
     }
